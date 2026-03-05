91df9735c12ca4e378701ab95341abed60223b0c
NolanDonahue <nolandonahue0@gmail.com>
2026-03-05 12:48:49 -0500

    Revised to an ID map for toggling active button. Added in additional button groups to include a clockwork reach calculator. Numbers are based off feel and not off an official listing.

--- src/App.js
+++ src/App.js
@@ -16,17 +16,42 @@ function App() {
     const [reach, setReach] = React.useState(0);
     const [requiredReach, setRequiredReach] = React.useState(21);
     const [playerCount, setPlayerCount] = React.useState(4);
+    const [selectedValue, setSelectedValue] = React.useState('player4');
 
-    const handlePlayerCountButtonPress = (event, newRequiredReach) => {
-        if (newRequiredReach != null) {
-            setRequiredReach(newRequiredReach)
-            setPlayerCount(event.target.innerText)
+    const idToValuesMap = {
+        'player2': { requiredReach: 17, playerCount: 2 },
+        'player3': { requiredReach: 18, playerCount: 3 },
+        'player4': { requiredReach: 21, playerCount: 4 },
+        'player5': { requiredReach: 25, playerCount: 5 },
+        'player6': { requiredReach: 28, playerCount: 6 },
+
+        '1clockwork1': { requiredReach: 8, playerCount: 1 },
+        '1clockwork2': { requiredReach: 12, playerCount: 2 },
+        '1clockwork3': { requiredReach: 14, playerCount: 3 },
+        '1clockwork4': { requiredReach: 16, playerCount: 4 },
+        
+        '2clockwork1': { requiredReach: 9, playerCount: 1 },
+        '2clockwork2': { requiredReach: 14, playerCount: 2 },
+        '2clockwork3': { requiredReach: 18, playerCount: 3 },
+        '2clockwork4': { requiredReach: 21, playerCount: 4 },
+    };
+
+    const handleToggleButtonChange = (event, newValue) => {
+        if (newValue !== null) {
+            setSelectedValue(newValue);
+            const values = idToValuesMap[newValue];
+            setRequiredReach(values.requiredReach);
+            setPlayerCount(values.playerCount);
+
+            console.log('selectedValue:', newValue);
+            console.log('requiredReach:', values.requiredReach);
+            console.log('playerCount:', values.playerCount);
         }
     }
 
     return (
         <Container maxWidth="sm" className="App"
-                   sx={{minWidth: "320px", height: "100%"}}
+                    sx={{minWidth: "320px", height: "100%"}}
         >
             <CssBaseline/>
             <Stack
@@ -43,8 +68,8 @@ function App() {
                     requiredReach={requiredReach}
                 />
                 <PlayerCountButtons
-                    requiredReach={requiredReach}
-                    handlePlayerCountButtonPress={handlePlayerCountButtonPress}
+                    selectedValue={selectedValue}
+                    handleToggleButtonChange={handleToggleButtonChange}
                 />
             </Stack>
         </Container>
--- src/components/PlayerCountButtons.js
+++ src/components/PlayerCountButtons.js
@@ -1,17 +1,49 @@
 import * as React from 'react';
 import {Stack, ToggleButton, ToggleButtonGroup} from "@mui/material";
 
-export const PlayerCountButtons = ({requiredReach, handlePlayerCountButtonPress}) => {
+export const PlayerCountButtons = ({selectedValue, handleToggleButtonChange}) => {
     return (
         <Stack justifyContent="center">
-            <h4>Players</h4>
-            <ToggleButtonGroup exclusive value={requiredReach} onChange={handlePlayerCountButtonPress}>
-                <ToggleButton value={17}>2</ToggleButton>
-                <ToggleButton value={18}>3</ToggleButton>
-                <ToggleButton value={21}>4</ToggleButton>
-                <ToggleButton value={25}>5</ToggleButton>
-                <ToggleButton value={28}>6</ToggleButton>
-            </ToggleButtonGroup>
+            <div class="container">
+                <h4>Players</h4>
+                <ToggleButtonGroup 
+                    exclusive 
+                    value={selectedValue} 
+                    onChange={handleToggleButtonChange}
+                    >
+                    <ToggleButton value='player2'>2</ToggleButton>
+                    <ToggleButton value='player3'>3</ToggleButton>
+                    <ToggleButton value='player4'>4</ToggleButton>
+                    <ToggleButton value='player5'>5</ToggleButton>
+                    <ToggleButton value='player6'>6</ToggleButton>
+                </ToggleButtonGroup>
+            </div>
+            <div class="container">
+                <h4>1 Clockwork</h4>
+                <ToggleButtonGroup 
+                    exclusive 
+                    value={selectedValue} 
+                    onChange={handleToggleButtonChange}
+                    >
+                    <ToggleButton value='1clockwork1'>1</ToggleButton>
+                    <ToggleButton value='1clockwork2'>2</ToggleButton>
+                    <ToggleButton value='1clockwork3'>3</ToggleButton>
+                    <ToggleButton value='1clockwork4'>4</ToggleButton>
+                </ToggleButtonGroup>
+            </div>
+            <div class="container">
+                <h4>2 Clockworks</h4>
+                <ToggleButtonGroup 
+                    exclusive 
+                    value={selectedValue} 
+                    onChange={handleToggleButtonChange}
+                    >
+                    <ToggleButton value='2clockwork1'>1</ToggleButton>
+                    <ToggleButton value='2clockwork2'>2</ToggleButton>
+                    <ToggleButton value='2clockwork3'>3</ToggleButton>
+                    <ToggleButton value='2clockwork4'>4</ToggleButton>
+                </ToggleButtonGroup>
+            </div>
         </Stack>
-    )
-}
+    );
+};
